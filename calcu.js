440fb7e4b2aed025443dea72d1b9f028ffcb27f6
Zainab <zainab@scaleablesolutions.com>
2022-09-24 16:20:20 +0500

    Calculator

---
+++ calcu.js
@@ -0,0 +1,18 @@
+let arr = [2,3,4,5,6];
+
+var sum = function add(a,b) {
+    return "Sum = "+(a+b);
+  };
+  var substract = function minus(a,b) {
+    return "Difference = "+(a-b);
+  };
+  var multiply = function product(a,b) {
+    return "Product = "+(a*b);
+  };
+  var divide = function div(a,b) {
+    return "Division = "+(a/b);
+  };
+  console.log(sum(6,2));
+  console.log(substract(6,2));
+  console.log(multiply(6,2));
+  console.log(divide(6,2));
